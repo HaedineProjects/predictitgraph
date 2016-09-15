c08f63f16413a699881ac62e145fb355a33901f2
carl <thunman@gmail.com>
2016-09-15 14:25:22 -0400

    first pass at predictit graph... a few tweaks remain

---
+++ read_probabilities.py
@@ -0,0 +1,12 @@
+import json
+from state_abbrevs import STATES
+
+f = open('current_probabilities.json')
+states = json.load(f)
+print states
+
+print 'state,value'
+for state in sorted(states):
+	d = states[state]['Democratic']
+	# r = state['Republican']
+	print STATES[state] + ',' + unicode(1.0 - d)
---
+++ update_probs.py
@@ -0,0 +1,34 @@
+import requests, time
+from state_urls import state_urls
+
+# # for state in state_urls:
+# 	# print state
+# 	# print state_urls[state]
+
+# AL = state_urls['AL']
+# r = requests.get(AL)
+# # print r.encoding
+# # print r.headers
+# response_data = r.json()
+# for k in response_data['Contracts']:
+# 	print k
+
+# print '\n\n\n\n\n\n\n'
+
+# for k in response_data:
+# 	print k
+# 	print k, response_data[k]
+	
+state_prices = {}
+for state in state_urls:
+	state_prices[state] = {}
+	r = requests.get(state_urls[state])
+	response_data = r.json()
+	
+	contracts = response_data['Contracts']
+	for c in contracts:
+		state_prices[state][c['Name']] = c['LastTradePrice']
+	time.sleep(5)
+
+
+print state_prices
